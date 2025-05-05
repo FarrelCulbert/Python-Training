6cc7350a9bb4d22d7c12b24ee684f9ff7d2caa1e
FarrelCulbert <farrel.culbert.21@student.ee.ittelkom-sby.ac.id>
2025-05-05 23:35:23 +0700

    day2

--- for_loops.py
+++ for_loops.py
@@ -1,3 +1,15 @@
 #for loops = execute a block of code a fized number of times.
 #            can iterate over a range, string, sequence, etc.
 
+# for x in range(1,11): #(start,end,step)
+#     print(x)
+
+# for x in reversed(range(1,11)):
+#     print(x)
+
+# for x in range(1,21):
+#     if x == 13:
+#         continue #skip the iteration (13)
+#         break #stop when the iteration (13)
+#     else:
+#         print(x)
