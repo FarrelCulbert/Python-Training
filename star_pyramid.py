70a02845e7c76a61a0bace81ef73b90d5b829d38
FarrelCulbert <farrel.culbert.21@student.ee.ittelkom-sby.ac.id>
2025-05-06 16:20:21 +0700

    day

--- star_pyramid.py
+++ star_pyramid.py
@@ -1,3 +1,15 @@
 n = int(input("enter number of rows: "))
-for i in range (1, n+1):
-    print("*" * i)
+#1
+# for i in range (1, n+1):
+#     print("*" * i)
+#2
+# for i in range (1, n+1):
+#      print("*" * (n - i))
+#3
+# for i in range (1, n+1):
+#      print(" " * (n - i), end="")
+#      print("*" * (2*i-1))
+#4
+for i in range (n,0,-1):
+     print(" " * (n - i), end="")
+     print("*" * (2*i-1))
